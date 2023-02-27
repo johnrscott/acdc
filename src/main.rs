62bfcdcc5cbe3abe4c6afbe76e254ecfcea226ea
John Scott <johnrscott0@gmail.com>
2023-02-27 20:42:09 +0000

    Added parsing of name_id

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,29 @@ fn parse_netlist_file(file_path: String) {
     let buffered = BufReader::new(input);
     
     for line in buffered.lines().map(|ln| ln.unwrap()) {
-	println!("{line}");
+	let mut tokens = line.split_ascii_whitespace();
+	// Get the component name and ID
+	let name_id = tokens.next()
+	    .expect("Failed to read component type and ID")
+	    .to_ascii_lowercase();
+	if name_id.len() < 2 {
+	    panic!("Parse error: invalid name_id {}", name_id);
+	}
+	let name = name_id.chars().nth(0).unwrap();
+	let id: String = name_id
+	    .chars()
+	    .skip(1)
+	    .collect();
+	let id: usize = id.parse()
+	    .unwrap_or_else(|error| {
+		panic!("Failed to parse ID {id} as unsigned integer ({error})")
+	    });
+	println!("Found name {name} and id {id}");
+	
+	
+	for token in tokens {
+	    println!("T: {token}");
+	}
 	
     }
 }
