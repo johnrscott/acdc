905ebc8c2bd08a705393ad83c1ebffaef5e40128
John Scott <johnrscott0@gmail.com>
2023-02-27 19:16:55 +0000

    Started adding reading from file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
-use csuperlu::{sparse_matrix::SparseMatrix, simple_driver::SimpleSystem, c::{stat::CSuperluStat, options::ColumnPermPolicy}, dense::DenseMatrix};
+use std::{fs::File, io::BufReader, io::BufRead};
 
 fn main() {
 
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Please provide exactly one argument (the matrix file path)");
+        std::process::exit(1);
+    }
+    let file_path = args[1].to_string();
+
+    let input = File::open(file_path)
+	.expect("Failed to open file");
+
+    let buffered = BufReader::new(input);
+    
+    for line in buffered.lines() {
+	println!("{}", line.unwrap());
+    }
+
+    
 }
