abcadc32506fb1c5cd29974e35f20620ef25be11
John Scott <johnrscott0@gmail.com>
2023-02-27 19:06:10 +0000

    Removed sparse example

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,4 @@ use csuperlu::{sparse_matrix::SparseMatrix, simple_driver::SimpleSystem, c::{sta
 
 fn main() {
 
-    let mut a = SparseMatrix::new(4, 4);
-    a.set_value(0, 0, 1.0);
-    a.set_value(1, 1, 1.0);
-    a.set_value(2, 2, 1.0);
-    a.set_value(3, 3, 1.0);
-
-    let a = a.compressed_column_format();
-    let b = vec![1.0; 4];
-    let b = DenseMatrix::from_vectors(4, 1, b);
-    
-    let system = SimpleSystem {
-	a,
-	b,
-    };
-
-    let mut stat = CSuperluStat::new();
-    
-    let solution = system.solve(&mut stat, ColumnPermPolicy::ColAMD).unwrap();
-
-    solution.x.print("Solution");
-    
 }
