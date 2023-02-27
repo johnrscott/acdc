3c085f46e393db88a06fc0715725bd882f21e285
John Scott <johnrscott0@gmail.com>
2023-02-27 18:47:28 +0000

    Added simple csuperlu example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use csuperlu::{sparse_matrix::SparseMatrix, simple_driver::SimpleSystem, c::{stat::CSuperluStat, options::ColumnPermPolicy}, dense::DenseMatrix};
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut a = SparseMatrix::new(4, 4);
+    a.set_value(0, 0, 1.0);
+    a.set_value(1, 1, 1.0);
+    a.set_value(2, 2, 1.0);
+    a.set_value(3, 3, 1.0);
+
+    let a = a.compressed_column_format();
+    let b = vec![1.0; 4];
+    let b = DenseMatrix::from_vectors(4, 1, b);
+    
+    let system = SimpleSystem {
+	a,
+	b,
+    };
+
+    let mut stat = CSuperluStat::new();
+    
+    let solution = system.solve(&mut stat, ColumnPermPolicy::ColAMD).unwrap();
+
+    solution.x.print("Solution");
+    
 }
