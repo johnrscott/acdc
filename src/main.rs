ede0ce1bb7a1c11291adc24851bf799b9cc242c3
John Scott <johnrscott0@gmail.com>
2023-02-27 19:47:59 +0000

    Moved parse to function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
 use std::{fs::File, io::BufReader, io::BufRead};
 
-fn main() {
-
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() != 2 {
-        println!("Please provide exactly one argument (the matrix file path)");
-        std::process::exit(1);
-    }
-    let file_path = args[1].to_string();
+fn parse_netlist_file(file_path: String) {
 
     let input = File::open(&file_path).unwrap_or_else(|error| {
 	panic!("Could not open file {file_path} ({})", error.kind());
@@ -19,6 +12,16 @@ fn main() {
 	println!("{line}");
 	
     }
+}
 
-    
+fn main() {
+
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Please provide exactly one argument (the matrix file path)");
+        std::process::exit(1);
+    }
+    let file_path = args[1].to_string();
+
+    parse_netlist_file(file_path);
 }
