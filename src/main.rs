f18b02104999ffbb2f999a0f88b0f7035e4e97b2
John Scott <johnrscott0@gmail.com>
2023-03-14 20:58:35 +0000

    Testing python bindings

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use crate::dc::LinearDcAnalysis;
 
-
 mod mna;
 mod dc;
 mod sparse;
 
 fn main() {
-
     let mut dc = LinearDcAnalysis::new();
     dc.add_resistor(1, 0, None, 50.0);
     dc.add_resistor(2, 1, None, 50.0);
