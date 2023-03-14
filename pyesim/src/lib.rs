9e9c7f308a801bd2e8bfc82ca57378221c59a1a2
John Scott <johnrscott0@gmail.com>
2023-03-14 21:19:04 +0000

    Got python bindings working

--- pyesim/src/lib.rs
+++ pyesim/src/lib.rs
@@ -1,3 +1,4 @@
+use libesim::dc::LinearDcAnalysis;
 use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
@@ -6,10 +7,22 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+#[pyfunction]
+fn test() {
+    let mut dc = LinearDcAnalysis::new();
+    dc.add_resistor(1, 0, None, 50.0);
+    dc.add_resistor(2, 1, None, 50.0);
+    dc.add_independent_voltage_source(2, 0, 0, 5.0);
+    let (voltages, currents) = dc.solve();
+    println!("{:?}", voltages);
+    println!("{:?}", currents);   
+
+}
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pyesim(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(test, m)?)?;
     Ok(())
 }
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod mna;
+pub mod dc;
+pub mod sparse;
