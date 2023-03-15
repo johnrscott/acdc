0f546f20d8c4289980823d8a248c4834dc35ecdf
John Scott <johnrscott0@gmail.com>
2023-03-15 19:13:08 +0000

    Added capacitor in AC analysis

--- src/ac.rs
+++ src/ac.rs
@@ -2,8 +2,10 @@
 
 use crate::mna::Mna;
 use num::Complex;
+use std::f64::consts::PI;
 
 pub struct LinearAcAnalysis {
+    omega: f64,
     mna: Mna<Complex<f64>>,
 }
 
@@ -11,6 +13,7 @@ impl LinearAcAnalysis {
     /// New AC analysis at radial frequency omega
     pub fn new(omega: f64) -> Self {
 	Self {
+	    omega,
 	    mna: Mna::new(),
 	}
     }
@@ -26,6 +29,19 @@ impl LinearAcAnalysis {
 	self.mna.add_resistor(term_1, term_2, current_edge, r);
     }
 
+    pub fn add_capacitor(
+	&mut self,
+	term_1: usize,
+	term_2: usize,
+	current_edge: Option<usize>,
+	capacitance: f64,
+    ) {
+	let s = Complex::i() * 2.0 * PI * self.omega;
+	let r = 1.0 / (s * capacitance);
+	self.mna.add_resistor(term_1, term_2, current_edge, r);
+    }
+
+    
     pub fn add_independent_voltage_source(
     	&mut self,
     	term_pos: usize,
