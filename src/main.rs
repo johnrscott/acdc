53034e0c9cf02e62419c85148dc46892d378a889
John Scott <johnrscott0@gmail.com>
2023-03-17 19:24:20 +0000

    Revert "Experimenting with egui"
    This reverts commit be5beecd811617d9a8aaaf87e7782592e7f88a99.

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ fn main() {
 }
 
 #[derive(Default)]
-struct SchematicApp {}
+struct MyEguiApp {}
 
-impl SchematicApp {
+impl MyEguiApp {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
@@ -18,7 +18,7 @@ impl SchematicApp {
     }
 }
 
-impl eframe::App for SchematicApp {
+impl eframe::App for MyEguiApp {
    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
        egui::CentralPanel::default().show(ctx, |ui| {
            ui.heading("Hello World!");
