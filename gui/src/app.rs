f9a7ed01471591260494a16f8c6268a644105cf3
John Scott <johnrscott0@gmail.com>
2023-03-18 12:46:20 +0000

    Added a resistor synbol, deleted dist files and updated gitignore

--- gui/src/app.rs
+++ gui/src/app.rs
@@ -1,5 +1,5 @@
 use eframe::emath::RectTransform;
-use egui::{Sense, Pos2, Rect, vec2, Stroke, Color32};
+use egui::{Sense, Pos2, Rect, vec2, Stroke, Color32, Painter};
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -40,13 +40,12 @@ impl TemplateApp {
 }
 
 impl eframe::App for TemplateApp {
-    /// Called by the frame work to save state before shutdown.
+    /// Called by the framework to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
     /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let Self { label, value } = self;
 
@@ -72,16 +71,67 @@ impl eframe::App for TemplateApp {
                 Rect::from_min_size(Pos2::ZERO - painter_proportions, 2. * painter_proportions),
                 response.rect,
             );
-
-	    let arrow_start = Pos2::new(0.0, 0.0);
-	    let arrow_direction = vec2(1.0, -1.0);
-	    let stroke_width = 1.;
-	    let color = Color32::WHITE;
-	    painter.arrow(
-                to_screen * arrow_start,
-                to_screen.scale() * arrow_direction,
-                Stroke::new(stroke_width, color),
-            );
+	    
+	    let resistor = Resistor::new();
+	    resistor.draw(&painter, to_screen * Pos2::new(0.0, 0.0));
 	});
+    }
+}
+
+/// The resistor symbol
+///
+/// This is the zig-zag resistor symbol. It comprises
+/// 6 equilateral triangles, alternating up and down (with
+/// a common base horizontal line), with a terminal segment
+/// at each end
+struct Resistor {
+    /// Length of base of equilateral triangle
+    x: f32,
+    /// Length of segment at each end of resistor
+    d: f32,
+    /// The line color and width
+    stroke: Stroke,
+}
+
+impl Resistor {
+
+    fn new() -> Self {
+	Self {
+	    x: 30.0,
+	    d: 60.0,
+	    stroke: Stroke {
+		width: 2.0,
+		color: Color32::WHITE,
+	    }
+	}
+    }
+
+    /// Draw either the right (reflect == 1.0) or the right
+    /// (reflect == -1.0) portion of the symbol
+    fn draw_half(&self, reflect: f32, painter: &Painter, origin: Pos2) {
+	let half_x = reflect * self.x/2.0;
+	let y = - reflect * self.x * f32::sqrt(3.0) / 2.0;
+	let start = origin;
+	let end = origin + vec2(half_x, -y);
+	painter.line_segment([start, end], self.stroke);
+	let start = end;
+	let end = end + vec2(2.0 * half_x, 2.0 * y);
+	painter.line_segment([start, end], self.stroke);
+	let start = end;
+	let end = end + vec2(2.0 * half_x, - 2.0 * y);
+	painter.line_segment([start, end], self.stroke);
+	let start = end;
+	let end = end + vec2(half_x, y);
+	painter.line_segment([start, end], self.stroke);
+	let start = end;
+	let end = end + vec2(reflect * self.d, 0.0);
+	painter.line_segment([start, end], self.stroke);
+	
+    }
+    
+    fn draw(&self, painter: &Painter, at: Pos2) {
+	self.draw_half(1.0, painter, at);
+	self.draw_half(-1.0, painter, at);
+
     }
 }
